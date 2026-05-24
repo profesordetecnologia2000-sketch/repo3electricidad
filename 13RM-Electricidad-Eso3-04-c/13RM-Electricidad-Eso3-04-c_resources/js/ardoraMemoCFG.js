30d5b9a8679e63109c9866dce76e2e4f37775505
profesordetecnologia2000-sketch <profesordetecnologia2000@gmail.com>
2026-05-24 09:45:16 +0200

    Add files via upload

---
+++ 13RM-Electricidad-Eso3-04-c/13RM-Electricidad-Eso3-04-c_resources/js/ardoraMemoCFG.js
@@ -0,0 +1,34 @@
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=360;timeIni=360; timeBon=0;
+var successes=0; successesMax=8; attempts=0; attemptsMax=99;
+var score=0;scoreMax=1;scoreInc=1; scoreDec=0;
+var typeGame=0;
+var tiTime=false;
+var tiTimeType=1;
+var tiButtonTime=true;
+var textButtonTime="Comenzar";
+var tiSuccesses=true;
+var tiAttempts=true;
+var tiScore=false;
+var startTime;
+var colorBack="#FFFDFD"; colorButton="#91962F"; colorText="#000000"; colorSele="#FF8000";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="Verdana, Geneva, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=5; messageOk="Correcto"; messageTime=""; messageError=""; messageErrorG=""; messageAttempts=""; isShowMessage=false;
+var urlOk=""; urlTime=""; urlError=""; urlAttempts="";
+var goURLOk="_blank"; goURLTime="_blank"; goURLAttempts="_blank"; goURLError="_blank"; 
+borderOk="#008000"; borderTime="#FF0000";borderError=""; borderAttempts="#FF0000";
+var wordsGame="MTNSTS1FbGVjdHJpY2lkYWQtRXNvMy0wNC1j"; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
+var wo1=["","","","","","","",""];im1=["bombilla1.jpg","conmutador1.jpg","interruptor1.jpg","diodo_led1.jpg","motor1.jpg","pila1.jpg","resistencia1.jpg","pulsador1.jpg"];mp31=["","","","","","","",""];alt1=["","","","","","","",""];
+var wo2=["","","","","","","",""];im2=["pila2.jpg","resistencia2.jpg","bombilla2.jpg","motor2.jpg","interruptor2.jpg","pulsador2.jpg","diodo_led2.jpg","conmutador2.jpg"];mp32=["","","","","","","",""];alt2=["","","","","","","",""];
+var cols=4; rows=4; items=8; itp=["MTA=","MTE=","MTI=","MTM=","MTQ=","MTU=","MTY=","MTc="]; pos=["MjI=","Mjc=","MjQ=","MjY=","MjM=","MjA=","MjE=","MjU="];
+var boardGame=[["","","",""],["","","",""],["","","",""],["","","",""]]; dirMedia="13RM-Electricidad-Eso3-04-c_resources/media/"
+var open1R=""; open1C=""; open2R=""; open2C=""; ansDo=0;
+var a;
