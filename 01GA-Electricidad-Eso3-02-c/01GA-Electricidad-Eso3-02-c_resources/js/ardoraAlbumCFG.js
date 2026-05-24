4afaf2b61f76847b03340b22686d134376a661d1
profesordetecnologia2000-sketch <profesordetecnologia2000@gmail.com>
2026-05-24 09:43:24 +0200

    Add files via upload

---
+++ 01GA-Electricidad-Eso3-02-c/01GA-Electricidad-Eso3-02-c_resources/js/ardoraAlbumCFG.js
@@ -0,0 +1,36 @@
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=120; timeIni=120; timeBon=20;
+var successes=0; successesMax=8; attempts=0; attemptsMax=99;
+var score=0; scoreMax=1; scoreInc=1; scoreDec=1
+var typeGame=0;
+var tiTime=false;
+var tiTimeType=1;
+var tiButtonTime=true;
+var textButtonTime="Començar";
+var tiSuccesses=true;
+var tiAttempts=true;
+var tiScore=true;
+var startTime;
+var colorBack="#FFFDFD"; colorButton="#91962F"; colorText="#000000"; colorSele="#FF8000";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=1; scoWrong=0; scoOkDo=0; scoWrongDo=1; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="Verdana, Geneva, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=5; messageOk="Correcto"; messageTime=""; messageError="Corrije los errores y pulsa el botón [?]"; messageAttempts=""; isShowMessage=false;
+var urlOk=""; urlTime=""; urlError=""; urlAttempts="";
+var goURLOk="_blank"; goURLTime="_blank"; goURLAttempts="_blank"; goURLError="_blank"; 
+borderOk="#008000"; borderTime="#FF0000";borderError="#FF0000"; borderAttempts="#FF0000";
+var imaSel=0; texSel=0;
+var board=[["01GA-Electricidad-Eso3-02-c_resources/media/1.jpg","01GA-Electricidad-Eso3-02-c_resources/media/2.jpg","01GA-Electricidad-Eso3-02-c_resources/media/3.jpg","01GA-Electricidad-Eso3-02-c_resources/media/4.jpg"],["01GA-Electricidad-Eso3-02-c_resources/media/5.jpg","01GA-Electricidad-Eso3-02-c_resources/media/6.jpg","01GA-Electricidad-Eso3-02-c_resources/media/7.jpg","01GA-Electricidad-Eso3-02-c_resources/media/8.jpg"],["0","0","0","0"],["0","0","0","0"]]; var balt=[["","","",""],["","","",""],["0","0","0","0"],["0","0","0","0"]]; var xAnswer=5; answer=[]; rows=6; cols=10; xKey=-1; yKey=-1; animationX=[]; animationY=[]; animationPosX=[]; animationPosY=[]; animationLetter=[];
+var words=[]; answers=[["bombilla","dido-led","motor","pulsador"],["resistencia","resistencia variable","selector","timbre"],["0","0","0","0"],["0","0","0","0"]]; letters=[]; indexLetters=0; xSel=-1; ySel=-1; iLetter=-1; jLetter=-1;
+var alt1=[["0","0","0","0"],["0","0","0","0"],["0","0","0","0"],["0","0","0","0"]];
+var alt2=[["0","0","0","0"],["0","0","0","0"],["0","0","0","0"],["0","0","0","0"]];
+var alt3=[["0","0","0","0"],["0","0","0","0"],["0","0","0","0"],["0","0","0","0"]];
+var alt4=[["0","0","0","0"],["0","0","0","0"],["0","0","0","0"],["0","0","0","0"]];
+var alt5=[["0","0","0","0"],["0","0","0","0"],["0","0","0","0"],["0","0","0","0"]];
+var wordsGame="MDFHQS1FbGVjdHJpY2lkYWQtRXNvMy0wMi1j"; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
---
+++ 03GP-Electricidad-Eso3-02-c/03GP-Electricidad-Eso3-02-c_resources/js/ardoraPuzleCFG.js
@@ -0,0 +1,32 @@
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=60; timeIni=60; timeBon=20;
+var successes=0; successesMax=12; attempts=0; attemptsMax=99;
+var score=0; scoreMax=1; scoreInc=1; scoreDec=1
+var typeGame=1;
+var tiTime=false;
+var tiTimeType=1;
+var tiButtonTime=true;
+var textButtonTime="Comenzar / Començar";
+var tiSuccesses=true;
+var tiAttempts=true;
+var tiScore=true;
+var startTime;
+var colorBack="#FFFDFD"; colorButton="#91962F"; colorText="#000000"; colorSele="#FF8000";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=5; messageOk="¡Correcto! / Correcte!"; messageTime=""; messageError="Corrige los errores y pulsa el botón [?] / Corregix els errors i polsa el botó [?]"; messageAttempts=""; isShowMessage=false;
+var urlOk=""; urlTime=""; urlError=""; urlAttempts="";
+var goURLOk="_blank"; goURLTime="_blank"; goURLAttempts="_blank"; goURLError="_blank"; 
+borderOk="#008000"; borderTime="#FF0000";borderError="#FF0000"; borderAttempts="#FF0000";
+var boardGame=[["0","0","0","0"],["0","0","0","0"],["0","0","0","0"]]; var imaSel=0; texSel=0;
+var original="03GP-Electricidad-Eso3-02-c_resources/media/original.jpg";
+var board=[["03GP-Electricidad-Eso3-02-c_resources/media/cGllY2UxXzE=.jpg","03GP-Electricidad-Eso3-02-c_resources/media/cGllY2UyXzE=.jpg","03GP-Electricidad-Eso3-02-c_resources/media/cGllY2UzXzE=.jpg","03GP-Electricidad-Eso3-02-c_resources/media/cGllY2U0XzE=.jpg"],["03GP-Electricidad-Eso3-02-c_resources/media/cGllY2UxXzI=.jpg","03GP-Electricidad-Eso3-02-c_resources/media/cGllY2UyXzI=.jpg","03GP-Electricidad-Eso3-02-c_resources/media/cGllY2UzXzI=.jpg","03GP-Electricidad-Eso3-02-c_resources/media/cGllY2U0XzI=.jpg"],["03GP-Electricidad-Eso3-02-c_resources/media/cGllY2UxXzM=.jpg","03GP-Electricidad-Eso3-02-c_resources/media/cGllY2UyXzM=.jpg","03GP-Electricidad-Eso3-02-c_resources/media/cGllY2UzXzM=.jpg","03GP-Electricidad-Eso3-02-c_resources/media/cGllY2U0XzM=.jpg"]]; var xAnswer=5; answer=[]; rows=6; cols=10; xKey=-1; yKey=-1; animationX=[]; animationY=[]; animationPosX=[]; animationPosY=[]; animationLetter=[];
+var words=[];letters=[]; indexLetters=0; xSel=-1; ySel=-1; iLetter=-1; jLetter=-1;
+var wordsGame="MDNHUC1FbGVjdHJpY2lkYWQtRXNvMy0wMi1j"; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
